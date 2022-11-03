2e00af697bd6899c33b889bbf93b9f90c2ffae0b
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-03 13:56:56 -0600

    modified:   Api_Htours/api/__pycache__/views.cpython-310.pyc 	modified:   Api_Htours/api/views.py

--- Api_Htours/api/views.py
+++ Api_Htours/api/views.py
@@ -1,9 +1,14 @@
 from django.views import View
 from .models import empleados
 from django.http.response import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 class EmpleadosView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
     def get(self,request):
         employes = list( empleados.objects.values())
         if len(employes)>0:
@@ -13,7 +18,9 @@ class EmpleadosView(View):
         return JsonResponse(datos)
 
     def post(self,request):
-        pass
+
+        datos = {'message':"Success"}
+        return JsonResponse(datos)
     def put(self,request):
         pass
     def delete(self,request):
