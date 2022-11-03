5c93ae3271be9ef51a06b0833174d416cf69d213
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-03 14:18:49 -0600

    metodo put

--- Api_Htours/api/urls.py
+++ Api_Htours/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import EmpleadosView
 urlpatterns=[
-    path('empleados/',EmpleadosView.as_view(),name='empleados_list')
+    path('empleados/',EmpleadosView.as_view(),name='empleados_list'),
+    path('empleados/<int:id>',EmpleadosView.as_view(),name='empleados_process')
 ]
--- Api_Htours/api/views.py
+++ Api_Htours/api/views.py
@@ -10,13 +10,24 @@ class EmpleadosView(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-    def get(self,request):
-        employes = list( empleados.objects.values())
-        if len(employes)>0:
-            datos = {'message':"Success","Empleados":employes}
+
+    def get(self,request,id=0):
+        if(id>0):
+            employes= list(empleados.objects.filter(id=id).values())
+            if len(employes) > 0:
+                employe= employes[0]
+                datos = {'message':"Success","Empleado: ":employe}
+            else:
+                datos = {'message':"Empleados no encontrados"}
+
+            return JsonResponse(datos)
         else:
-            datos = {'message':"Empleado no encontrado"}
-        return JsonResponse(datos)
+            employes = list( empleados.objects.values())
+            if len(employes)>0:
+                datos = {'message':"Success","Empleados":employes}
+            else:
+                datos = {'message':"Empleado no encontrado"}
+            return JsonResponse(datos)
 
     def post(self,request):
         # print(request.body)
@@ -26,7 +37,19 @@ class EmpleadosView(View):
         datos = {'message':"Success"}
         return JsonResponse(datos)
 
-    def put(self,request):
-        pass
+    def put(self,request,id):
+        jd = json.loads(request.body)
+        employes= list(empleados.objects.filter(id=id).values())
+
+        if len(employes) > 0:
+            empleado = empleados.objects.get(id=id)
+            empleado.name=jd['Name']
+            empleado.edad=jd['Edad']
+            empleado.save()
+            datos = {'message':"Success"}
+        else:
+            datos = {'message':"Empleado no encontrado"}
+        return JsonResponse(datos)
+
     def delete(self,request):
         pass
