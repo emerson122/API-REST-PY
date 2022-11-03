fdbff4101959c35d16be5b24975b2ac40d9481c2
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-03 13:49:13 -0600

    Actualizacion 2.0

---
+++ Api_Htours/api/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import EmpleadosView
+urlpatterns=[
+    path('empleados/',EmpleadosView.as_view(),name='empleados_list')
+]
--- Api_Htours/api/views.py
+++ Api_Htours/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.views import View
+from .models import empleados
+from django.http.response import JsonResponse
 # Create your views here.
+
+class EmpleadosView(View):
+    def get(self,request):
+        employes = list( empleados.objects.values())
+        if len(employes)>0:
+            datos = {'message':"Success","Empleados":employes}
+        else:
+            datos = {'message':"Empleado no encontrado"}
+        return JsonResponse(datos)
+
+    def post(self,request):
+        pass
+    def put(self,request):
+        pass
+    def delete(self,request):
+        pass
